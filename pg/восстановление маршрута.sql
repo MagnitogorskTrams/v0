2175e57882b5fbbb4a9a120261781b86c54c14b1
Антон А.Беличков <belichkov.aa@mmk.ru>
2016-04-18 09:51:30 +0300

    исправлен и дополнен комментарий

--- pg/восстановление маршрута.sql
+++ pg/восстановление маршрута.sql
@@ -1,5 +1,7 @@
---попытка восстановить маршрут по первому времени прохождения через 
-остановки
+/*
+попытка восстановить маршрут по первому времени прохождения через остановки
+только будние дни и без учета модификаций маршрута
+*/
 SELECT st.name, min(sch."time") as t 
 FROM schedules sch INNER JOIN routes r ON r.id = sch.route_id
 INNER JOIN stops st ON st.id = sch.stop_id
