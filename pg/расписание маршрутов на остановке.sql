30e2eaf9f9b0881d1e1e602d291ecf6afdf4f4af
Anton Belichkov <az09.mgn@gmail.com>
2016-05-10 00:51:12 +0500

    запрос для получения расписания интересующих маршрутов через выбранную остановку

---
+++ pg/расписание маршрутов на остановке.sql
@@ -0,0 +1,3 @@
+﻿/*выбираем расписание интересующих маршрутов через выбранную остановку*/
+select * from stop_schedule((select id from stops where name like '%Береговая%' and direction not like '%ТЭЦ%' limit 1))
+where route_name in ('7','20','22','33','6') order by "time";
